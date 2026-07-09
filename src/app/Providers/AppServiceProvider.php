2e12163bbd427556becf6f389d3892e9247b4d7f
kiseolar-ruslan <kretsu.rus@gmail.com>
2026-07-09 20:52:42 +0300

    completed blade lesson

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -21,5 +21,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         View::share('date', date('Y'));
+
+//        View::composer('user*', function ($view) {
+//            $view->with('balance', 123);
+//        });
     }
 }
